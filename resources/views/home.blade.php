f925fbdd23df72f14d64bc4774117a17961ec644
Артур <artur.archevodov@gmail.com>
2023-06-27 16:18:56 +0300

    Новая модель - сообщения + строка ввода сообщения

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        Здесь будет чат...
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="{{ __("Enter a message") }}" aria-label="{{ __("Enter a message") }}" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <input class="btn btn-outline-secondary rounded-start-0" type="submit" value="{{ __("Send") }}">
+            </div>
+        </div>
     </div>
 </div>
 @endsection
