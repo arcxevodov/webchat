7e3e3837e3633ab8a23f4c08fc5181ce01497990
Артур <artur.archevodov@gmail.com>
2023-06-29 20:33:37 +0300

    User Info

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
-
+@php($messages_count = \App\Models\Message::where('user_id', '=', $user->id)->count())
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            {{ $user->name }}
-            {{ $user->email }}
+    <div class="container mt-5">
+        <h1>Информация о пользователе</h1>
+        <div class="d-flex mt-5">
+            <img src="{{ asset($user->avatar) }}" alt="User" class="rounded-circle d-flex align-self-start me-3 shadow-sm">
+            <div class="row m-5">
+                <p><b>Имя: </b>{{ $user->name }}</p>
+                <p><b>E-Mail: </b>{{ $user->email }}</p>
+                <p><b>Сообщений отправлено: </b>{{ $messages_count }}</p>
+            </div>
         </div>
     </div>
 @endsection
