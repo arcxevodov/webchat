de7dc11f1b1d6a2638009799e4df2e5a319227fe
Артур <artur.archevodov@gmail.com>
2023-06-29 16:09:35 +0300

    Message Field Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="input-group mb-3">
-            <input type="text" name="text" id="message" class="form-control rounded border-warning warning" placeholder="{{ __("Enter a message") }}" aria-label="{{ __("Enter a message") }}" aria-describedby="basic-addon2">
+        <div class="input-group mb-3 px-0">
+            <input type="text" name="text" id="message" class="form-control rounded border-warning warning me-3" placeholder="{{ __("Enter a message") }}" aria-label="{{ __("Enter a message") }}" aria-describedby="basic-addon2">
             <div class="input-group-append">
                 <button
                     id="sendButton"
-                    class="btn btn-warning rounded mx-3 text-white"
+                    class="btn btn-warning rounded text-white"
                     type="submit"
                     onclick="messageSend('{{ route('send') }}')">{{ __("Send") }}</button>
             </div>
