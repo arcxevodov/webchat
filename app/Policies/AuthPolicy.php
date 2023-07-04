230a3fbcebe02f082fa458a7af41ffd95de53955
Артур <artur.archevodov@gmail.com>
2023-07-04 13:33:09 +0300

    delete message middleware

---
+++ app/Policies/AuthPolicy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Message;
+use App\Models\User;
+
+class AuthPolicy
+{
+    public function deleteMessage(User $user, Message $message): bool
+    {
+        return $user->id === $message->user_id;
+    }
+}
