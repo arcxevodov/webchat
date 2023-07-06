5ca292bf18bbd0c7725e777dd10d971fa4c18a7e
Артур <artur.archevodov@gmail.com>
2023-07-06 15:05:38 +0300

    refactoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,50 @@
 import './bootstrap';
+$('#editMessageForm').on('submit', (e) => {
+    e.preventDefault();
+    let inputText = $('#editMessageForm textarea').val()
+    alert(inputText)
+    $.ajax({
+        url: $('#editUrl').val(),
+        method: 'patch',
+        dataType: 'html',
+        data: {
+            _token: $("input[name='_token']").val(),
+            text: inputText
+        },
+        success: function (data) {
+            $('#messages').load('#messages .messages-divs')
+            $('#message').val('')
+        }
+    })
+})
+$('#deleteMessageForm').on('submit', (e) => {
+    e.preventDefault();
+    $.ajax({
+        url: $('#deleteUrl').val(),
+        method: 'delete',
+        dataType: 'html',
+        data: {
+            _token: $("input[name='_token']").val()
+        },
+        success: function () {
+            $('#messages').load('#messages .messages-divs')
+        }
+    });
+})
+$('#sendMessageForm').on('submit', (e) => {
+    e.preventDefault();
+    let inputText = $('#message').val()
+    $.ajax({
+        url: $('#messageUrl').val(),
+        method: 'post',
+        dataType: 'html',
+        data: {
+            _token: $("input[name='_token']").val(),
+            text: inputText
+        },
+        success: function (data) {
+            $('#messages').load('#messages .messages-divs')
+            $('#message').val('')
+        },
+    })
+})
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -188,56 +187,5 @@
             @yield('content')
         </main>
     </div>
-    <script>
-        $('#editMessageForm').on('submit', (e) => {
-            e.preventDefault();
-            let inputText = $('#editMessageForm textarea').val()
-            alert(inputText)
-            $.ajax({
-                url: $('#editUrl').val(),
-                method: 'patch',
-                dataType: 'html',
-                data: {
-                    _token: $("input[name='_token']").val(),
-                    text: inputText
-                },
-                success: function (data) {
-                    $('#messages').load('#messages .messages-divs')
-                    $('#message').val('')
-                }
-            })
-        })
-        $('#deleteMessageForm').on('submit', (e) => {
-            e.preventDefault();
-            $.ajax({
-                url: $('#deleteUrl').val(),
-                method: 'delete',
-                dataType: 'html',
-                data: {
-                    _token: $("input[name='_token']").val()
-                },
-                success: function () {
-                    $('#messages').load('#messages .messages-divs')
-                }
-            });
-        })
-        $('#sendMessageForm').on('submit', (e) => {
-            e.preventDefault();
-            let inputText = $('#message').val()
-            $.ajax({
-                url: $('#messageUrl').val(),
-                method: 'post',
-                dataType: 'html',
-                data: {
-                    _token: $("input[name='_token']").val(),
-                    text: inputText
-                },
-                success: function (data) {
-                    $('#messages').load('#messages .messages-divs')
-                    $('#message').val('')
-                },
-            })
-        })
-    </script>
 </body>
 </html>
